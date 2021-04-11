500fa17c162117ea897dc523ae77b366ebd90128
Manoj Kumar <dharmakumar338@gmail.com>
2021-04-11 20:27:08 +0530

    Display a List  - and view selected hero in detail (as in step one. see the commit previous to this) added styling to heroes.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,26 @@
+import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-
   hero: Hero = {
-    id:1,
-    name:"Windstorm"
+    id: 1,
+    name: 'Windstorm',
+  };
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
