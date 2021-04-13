5f6979b06b4f40cabbb5487af2a7e8572ed7ae9a
Manoj Kumar <dharmakumar338@gmail.com>
2021-04-13 07:28:31 +0530

    Add Navigation added routing, dashboard and modified a bit of css and logic around dashboards

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
-import { MessageService } from './../message.service';
-import { HeroService } from './../hero.service';
-import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,23 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
-  heroes: Hero[] = [];
+  heroes: Hero[];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
-
-  ngOnInit(): void {
-    this.getHeroes();
   }
 }
